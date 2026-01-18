ee7088373163b035c07ebd70aa4493dfa3ca542a
verepcode <verepali@gmail.com>
2026-01-18 13:52:21 +0300

    feat(Utils): completed rpyJacobianAxisAngle method

--- include/es_ekf/Utils.hpp
+++ include/es_ekf/Utils.hpp
@@ -18,16 +18,18 @@ namespace es_ekf{
     inline double angleNormalize(double angle) {
         return std::remainder(angle, 2 * M_PI);  // returns angle between (-pi,pi]
     }
-    inline Eigen::Matrix3d rpyJacobianAxisAngle(const Eigen::Vector3d a){
+    inline Eigen::Matrix3d rpyJacobianAxisAngle(const Eigen::Vector3d& a){
+        //Jacobian of RPY Euler angles with respect to axis-angle vector.
         double na = a.norm();
-        double na * na * na;
-        Eigen:Vector3d u = a / t;
+        double t = a.norm();
+        double na3 = na * na * na;
+        Eigen::Vector3d u = a / t;
 
         //Jr 3x4 bir matris
-        Eigen::Matrix3d Jr = Eigen::Matrix3d::Zero();
+        Eigen::Matrix<double, 3, 4> Jr = Eigen::Matrix<double, 3, 4>::Zero();
         double denom0 = t*t * u[0]*u[0] + 1;
         double denom1 = sqrt(std::max(0.01, 1 - t*t * u[1]*u[1]));
-        double denom2 = t*t * u[2] + 1;
+        double denom2 = t*t * u[2]*u[2] + 1;
         Jr(0,0) = t/denom0;
         Jr(0,3) = u[0]/denom0;
         Jr(1,1) = t/denom1;
@@ -36,7 +38,11 @@ namespace es_ekf{
         Jr(2,3) = u[2]/denom2;
 
         //Ja 4x3 bir matris
-        Eigen::Matrix3d Ja = Eigen::Matrix3d::Zero();
-        
+        Eigen::Matrix<double, 4, 3> Ja = Eigen::Matrix<double, 4, 3>::Zero();
+        Ja << (a[1]*a[1] + a[2]*a[2])/na3,               -a[0]*a[1]/na3,              -a[0]*a[2]/na3,
+                        -a[0]*a[1]/na3,  (a[0]*a[0] + a[2]*a[2])/na3,              -a[1]*a[2]/na3,
+                        -a[0]*a[2]/na3,               -a[1]*a[2]/na3, (a[0]*a[0] + a[1]*a[1])/na3,
+                                a[0]/na,                      a[1]/na,                     a[2]/na;
+        return Jr * Ja;
     }
 }
