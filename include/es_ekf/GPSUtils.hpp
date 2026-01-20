2269ffd06feef707a66ceb3b9c7b8585eba52b7e
verepcode <verepali@gmail.com>
2026-01-21 00:28:58 +0300

    feat(GPSUtils): add constructor and started a setter

--- include/es_ekf/GPSUtils.hpp
+++ include/es_ekf/GPSUtils.hpp
@@ -4,10 +4,36 @@
 #include <optional>
 #include <Eigen/Dense>
 
-class GPSUtils {
+namespace es_ekf{
 
+class GPSUtils
+{
+public:
+    GPSUtils()
+        : a_(6378137.0)
+        , b_(6356752.314245)
+        , a2_(a_ * a_)
+        , b2_(b_ * b_)
+        , e2_(1.0 - (b2_ / a2_))
+        , e_(e2_ / (1.0 - e2_))
+        , R_(Eigen::Matrix3d::Identity())
 
-private:
+    {}
+    void setENUOrigin(const double& lat_zero, const double& lon_zero, const double& height_zero)
+    {
+        auto origin = geo2Ecef(lat_zero, lon_zero, height_zero);
+        auto x_zero = origin[0];
+        auto y_zero = origin[1];
+        auto z_zero = origin[2];
+        // auto o_zero = origin;
+
+    }
+    Eigen::Vector3d geo2Ecef(const double &lat, const double &lon, const double& height){
+
+    }
+
+    private :
+    // WGS84 constants
     const double a_;
     const double b_;
     const double a2_;
@@ -15,6 +41,7 @@ private:
     const double e2_;
     const double e_;
 
+    // Local ENU origin
     std::optional<double> lat_zero_;
     std::optional<double> lon_zero_;
     std::optional<double> hgt_zero_;
@@ -23,4 +50,5 @@ private:
     std::optional<double> z_zero_;
 
     Eigen::Matrix3d R_;
+};
 }
