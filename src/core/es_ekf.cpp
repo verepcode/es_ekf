1ccbc0e3d1d969e3c21fa88bc251bc8a224a143e
verepcode <verepali@gmail.com>
2026-02-02 17:38:19 +0300

    feat(es_ekf): initialized jacobian matrices

--- src/core/es_ekf.cpp
+++ src/core/es_ekf.cpp
@@ -1,15 +1,30 @@
-#include  <es_ekf/types.h>
+#include  "es_ekf/Types.hpp"
+#include <Eigen/Dense>
 
-class StateEstimator {
+class StateEstimator 
+{
 
-    public:
-        StateEstimator();
-        ~StateEstimator();
+public:
+    StateEstimator()
+    : gravity_(-9.81)
+    , l_jacobian_(Eigen::Matrix<double, 9, 6>::Zero())
+    , m_jacobian_(Eigen::Matrix3d::Identity())
+    , h_jacobian_(Eigen::Matrix<double, 3, 9>::Zero())
+    , p_est_(Eigen::MatrixXd::Zero())
 
-        struct PredictionResult prediction_model( data_for_prediction, gnss_data);
 
-    private:
 
-    
+{}
 
-}
+private:
+
+    double gravity_ ;
+    Eigen::MatrixXd l_jacobian_;
+    Eigen::MatrixXd m_jacobian_;
+    Eigen::MatrixXd h_jacobian_;
+    Eigen::MatrixXd p_est_, v_est_;
+
+
+
+
+};
