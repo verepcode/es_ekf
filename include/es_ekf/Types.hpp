ea728510d5baf1cb0c51bd8b6759ce980f48ed70
verepcode <verepali@gmail.com>
2026-02-05 21:39:52 +0300

    feat(Types): added Imu, GNSS data

--- include/es_ekf/Types.hpp
+++ include/es_ekf/Types.hpp
@@ -3,15 +3,34 @@
 
 #include <Eigen/Dense>
 
-struct PredictionResult{
-    Eigen::VectorXd nominal_state;
-    Eigen::VectorXd error_state;
-    Eigen::VectorXd covariance;
-};
+namespace es_ekf{
 
-struct UpdateResult{
-    Eigen::VectorXd state;
-    Eigen::VectorXd covariance;
-    Eigen::VectorXd innovation;
-    Eigen::VectorXd kalman_gain;
-};
+    struct ImuData
+    {
+        Eigen::Vector3d linear_accel;
+        Eigen::Vector3d ang_vel;
+        double timestamp;
+    };
+
+    struct GNSSData
+    {
+        Eigen::Vector3d position;
+        Eigen::Vector3d covariance;
+        double timestamp;
+    };
+
+    struct PredictionResult
+    {
+        Eigen::VectorXd nominal_state;
+        Eigen::VectorXd error_state;
+        Eigen::VectorXd covariance;
+    };
+
+    struct UpdateResult
+    {
+        Eigen::VectorXd state;
+        Eigen::VectorXd covariance;
+        Eigen::VectorXd innovation;
+        Eigen::VectorXd kalman_gain;
+    };
+}
